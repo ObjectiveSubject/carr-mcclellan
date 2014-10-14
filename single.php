8262d4135332342ab37efd6bd3e4ab0c144d921c
Dave Kellam <dave@eightface.com>
2014-10-14 18:25:46 -0400

    Consolidate components for single post into one file

--- single.php
+++ single.php
@@ -21,12 +21,52 @@ get_header(); ?>
 		</header><!-- .entry-header -->
 
 		<main id="main" class="site-main span12 aligncenter clear" role="main">
-			
-				<?php get_template_part( 'aside', 'author-share' ); ?>
-	
-				<?php get_template_part( 'content', 'single' ); ?>
-				
-				<?php get_template_part( 'aside', 'categories' ); ?>
+
+				<aside class="aside aside-left aside-author-share">
+
+					<div class="border-block top author">
+						<h3 class="block-label">Author</h3>
+						<ul>
+							<?php $related_attorneys = get_post_meta( $post->ID, 'post_attorneys', 'single' ); ?>
+							<?php foreach ( $related_attorneys as $related_attorney ) { ?>
+								<li><a href="<? echo get_permalink( $related_attorney ); ?>" class="link-gray3"><? echo get_the_title( $related_attorney ); ?></a></li>
+							<?php } ?>
+						</ul>
+					</div>
+
+					<div class="border-block top social-share">
+						<h3 class="block-label">Share</h3>
+						<ul>
+							<li><a href="#" class="social-icon icon-twitter"><span class="hide-text">Twitter</span></a></li>
+							<li><a href="#" class="social-icon icon-linkedin"><span class="hide-text">Linkedin</span></a></li>
+							<li><a href="#" class="social-icon icon-facebook"><span class="hide-text">Facebook</span></a></li>
+							<li><a href="#" class="social-icon icon-tumblr"><span class="hide-text">Tumblr</span></a></li>
+							<li><a href="#" class="social-icon icon-googleplus"><span class="hide-text">Google Plus</span></a></li>
+						</ul>
+					</div>
+				</aside>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class('span9 push-left'); ?>>
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div><!-- .entry-content -->
+
+				</article><!-- #post-## -->
+
+				<aside class="aside aside-right aside-categories">
+
+					<div class="border-block top categories">
+						<h3 class="block-label">Categories</h3>
+						<ul>
+							<?php $cats = get_the_category(); ?>
+							<?php foreach ( $cats as $cat ) { ?>
+								<li><a href="<?php echo get_category_link( $cat->term_id ); ?>" class="link-gray3"><?php echo $cat->name; ?></a></li>
+							<?php } ?>
+						</ul>
+					</div>
+
+				</aside>
 	
 		</main><!-- #main -->
 		
