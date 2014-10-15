0f8be73947f21123dc53af8e5b03c445f93f57af
Dave Kellam <dave@eightface.com>
2014-10-15 16:00:55 -0400

    Removing page-home, tweaks to front-page

--- front-page.php
+++ front-page.php
@@ -1,25 +1,31 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package Carr McClellan
+ * The template for the home page
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
 
-		Front Page
+	<header class="page-header">
+		<div class="centered">
+			<?php get_template_part('t-site-branding'); ?>
+			<h1 class="page-title">We Make<br/>It Happen</h1>
+			<div class="page-subtitle"><?php the_content(); ?></div>
+		</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<main id="main" class="site-main span12 aligncenter" role="main">
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+			</main><!-- #main -->
+
+		<?php endwhile; // end of the loop. ?>
+	</header>
+
+</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
