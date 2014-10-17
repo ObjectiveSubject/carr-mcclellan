014f8182565617ff2223b8c1ced56419fbfc2419
Dave Kellam <dave@eightface.com>
2014-10-17 13:59:12 -0400

    First pass at recruiting template

--- t-recruiting.php
+++ t-recruiting.php
@@ -5,56 +5,53 @@
 	get_header();
 ?>
 
-<body>
-	<div id="wrapper">
-		<div class="w1">
-			<div class="w2">
-				<div class="container">
-					<header id="header" class="header-4 header-our-firm-detail">
-						<div class="panel">
-							<span class="addthis link-share">Share</span>
-						</div>
-						<h1><?=get_the_title();?></h1>
-					</header>
-					
-					<div id="main">
-						<section id="content">
-							<div class="head-text">
-								<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-								<?php the_content(); ?>
-								<?php endwhile; ?>
-								<?php endif; ?>
-							</div>
-							<div class="vacancies-list">
-								<article class="item">
-									<h2 class="block-1 last">Directors</h2>
-									<div class="holder">
-										<?php the_block('Directors'); ?>
-									</div>
-								</article>
-								<article class="item">
-									<h2 class="block-1 last">Associates</h2>
-									<div class="holder">
-										<?php the_block('Associates'); ?>
-									</div>
-								</article>
-								<article class="item">
-									<h2 class="block-1 last">Paralegals &amp; Staff</h2>
-									<div class="holder">
-										<?php the_block('Paralegals Staff'); ?>
-									</div>
-								</article>
-								<article class="item">
-									<h2 class="block-1 last">Recruiting Contacts</h2>
-									<div class="holder">
-										<?php the_block('Recruiting Contacts'); ?>
-									</div>
-								</article>
-							</div>
-						</section>
-					</div>					
-				</div>
-			</div>
+	<header class="page-header">
+		<div class="span12 aligncenter">
+			<h1 class="page-title"><?php the_title(); ?></h1>
 		</div>
-		
-<?php get_footer(); ?>
+	</header>
+
+	<main id="main" class="site-main span6 aligncenter clear" role="main">
+
+		<section id="content">
+			<div class="head-text community-head-text">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+
+			<?php the_block('Main Body'); ?>
+
+			<div class="vacancies-list">
+				<article class="item">
+					<h2 class="block-1 last">Directors</h2>
+					<div class="holder">
+						<?php the_block('Directors'); ?>
+					</div>
+				</article>
+				<article class="item">
+					<h2 class="block-1 last">Associates</h2>
+					<div class="holder">
+						<?php the_block('Associates'); ?>
+					</div>
+				</article>
+				<article class="item">
+					<h2 class="block-1 last">Paralegals &amp; Staff</h2>
+					<div class="holder">
+						<?php the_block('Paralegals Staff'); ?>
+					</div>
+				</article>
+				<article class="item">
+					<h2 class="block-1 last">Recruiting Contacts</h2>
+					<div class="holder">
+						<?php the_block('Recruiting Contacts'); ?>
+					</div>
+				</article>
+			</div>
+
+		</section>
+	</main>
+
+<?php get_footer();
+
