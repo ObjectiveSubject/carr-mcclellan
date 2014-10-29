e224c85d2c648141fdef6a9f5c326c7fcd17fafd
Dave Kellam <dave@eightface.com>
2014-10-29 18:25:10 -0400

    Reformatting reg_milestones.php for WP code standards

--- inc/reg_milestones.php
+++ inc/reg_milestones.php
@@ -1,35 +1,36 @@
 <?php
 
 /*********************************************************
-  Register Custom Post Type
-*/
+ * Register Custom Post Type
+ */
 
-  add_action('init', 'milestones_register');
-  function milestones_register() {
-      $args = array(
-          'label' => __('Firm History'),
-          'singular_label' => __('Milestones'),
-          'public' => false,
-          'show_ui' => true,
-          'capability_type' => 'post',
-          'hierarchical' => false,
-          'rewrite' => true,
-          'supports' => array('title', 'editor', 'thumbnail','page-attributes')
-        );
-      register_post_type( 'milestones' , $args );
-  }
+add_action( 'init', 'milestones_register' );
+function milestones_register() {
+	$args = array(
+		'label'           => __( 'Firm History' ),
+		'singular_label'  => __( 'Milestones' ),
+		'public'          => false,
+		'show_ui'         => true,
+		'capability_type' => 'post',
+		'hierarchical'    => false,
+		'rewrite'         => true,
+		'supports'        => array( 'title', 'editor', 'thumbnail', 'page-attributes' )
+	);
+	register_post_type( 'milestones', $args );
+}
 
 
 /*********************************************************
-  Custom Column Headers
-*/
+ * Custom Column Headers
+ */
 
-  add_filter("manage_edit-milestones_columns", "milestones_edit_columns");
+add_filter( "manage_edit-milestones_columns", "milestones_edit_columns" );
 
-  function milestones_edit_columns($columns){
-        $columns = array(
-            "cb" => "<input type=\"checkbox\" />",
-            "title" => "Title"
-        );
-        return $columns;
-  }
+function milestones_edit_columns( $columns ) {
+	$columns = array(
+		"cb"    => "<input type=\"checkbox\" />",
+		"title" => "Title"
+	);
+
+	return $columns;
+}
