fa95aeac5c059c1764eee2829a70fc1eaf9c93c7
Dave Kellam <dave@eightface.com>
2014-10-29 18:22:38 -0400

    Change label of Milestones, remove metabox cruft; fixes #16

--- inc/reg_milestones.php
+++ inc/reg_milestones.php
@@ -7,7 +7,7 @@
   add_action('init', 'milestones_register');
   function milestones_register() {
       $args = array(
-          'label' => __('Milestones'),
+          'label' => __('Firm History'),
           'singular_label' => __('Milestones'),
           'public' => true,
           'show_ui' => true,
@@ -19,89 +19,17 @@
       register_post_type( 'milestones' , $args );
   }
 
-/*********************************************************
-  Custom Meta Box
-*/
-
-  add_action("admin_init", "admin_init");
-  add_action( 'save_post', 'save_milestone_meta', 15, 2 );
-
-  function milestones_meta_options(){
-    global $post;
-    $custom = get_post_custom($post->ID);
-    $milestone_date = "";
-    if( $custom["milestone_date"][0] ){
-      $milestone_date = date('F Y', $custom["milestone_date"][0]);
-    }
-  ?>
-
-  <table class="form-table">
-    <tr>
-      <th style="width: 20%;"><label for="milestone_date">Milestone Date</label></th>
-      <td>
-        <input type="text" class="text_input" name="milestone_date" value="<?php echo $milestone_date; ?>" style="width:97%" /><br />
-        <small>Ex: January 1988</small>
-      </td>
-    </tr>
-	<?php wp_nonce_field( 'save', 'carr_milestones' ); ?>
-  </table>
-
-
-  <?php
-  }
-
-  function save_milestone_meta( $post_id, $post ){
-	// Bail if doing autosave
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) { return; }
-
-	// Bail if nonce isn't set
-	if ( ! isset( $_POST['carr_milestones'] ) || ! wp_verify_nonce( $_POST['carr_milestones'], 'save' ) ) { return; }
-
-	// Bail if the user isn't allowed to edit the post
-	if ( ! current_user_can( 'edit_post', $post_id ) ) { return; }
-
-	// Bail if not an asset
-	if ( 'milestones' !== $post->post_type ) { return; }
-
-    if( isset( $_POST['milestone_date'] ) ) {
-	    $new_date = strtotime( $_POST['milestone_date'] );
-
-	    update_post_meta( $post_id, 'milestone_date', $new_date );
-    } else {
-	    delete_post_meta( $post_id, 'milestone_date' );
-    }
-  }
-
 
 /*********************************************************
   Custom Column Headers
 */
 
   add_filter("manage_edit-milestones_columns", "milestones_edit_columns");
-  add_action("manage_posts_custom_column",  "milestones_custom_columns");
 
   function milestones_edit_columns($columns){
         $columns = array(
             "cb" => "<input type=\"checkbox\" />",
-            "title" => "Milestone Title",
-            "milestone_date" => "Milestone Date"
+            "title" => "Title"
         );
         return $columns;
   }
-
-  function milestones_custom_columns($column){
-        global $post;
-        switch ($column)
-        {
-            case "milestone_date":
-                $custom = get_post_custom();
-                $milestone_date = "";
-                if( $custom["milestone_date"][0] ){
-                  $milestone_date = date('F Y', $custom["milestone_date"][0]);
-                }
-                echo $milestone_date;
-                break;
-        }
-  }
-
-?>
