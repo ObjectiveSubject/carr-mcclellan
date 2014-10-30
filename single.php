64834df60ea25a475a5d76b2beb716fb8763472a
Dave Kellam <dave@eightface.com>
2014-10-30 18:48:27 -0400

    Early attempt at wiring up sharing icons

--- single.php
+++ single.php
@@ -41,11 +41,12 @@ get_header(); ?>
 					<div class="border-block top social-share">
 						<h3 class="block-label">Share</h3>
 						<ul>
-							<li><a href="#" class="social-icon icon-twitter"><span class="hide-text">Twitter</span></a></li>
-							<li><a href="#" class="social-icon icon-linkedin"><span class="hide-text">Linkedin</span></a></li>
-							<li><a href="#" class="social-icon icon-facebook"><span class="hide-text">Facebook</span></a></li>
-							<li><a href="#" class="social-icon icon-tumblr"><span class="hide-text">Tumblr</span></a></li>
-							<li><a href="#" class="social-icon icon-googleplus"><span class="hide-text">Google Plus</span></a></li>
+							<?php // @TODO These need some work and more testing ?>
+							<li><a href="https://twitter.com/share?url=<?php echo get_the_permalink(); ?>&title=<?php echo get_the_title(); ?>" class="social-icon icon-twitter"><span class="hide-text">Twitter</span></a></li>
+							<li><a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_the_permalink(); ?>&title=<?php echo get_the_title(); ?>&source=carrmcclellan.com" class="social-icon icon-linkedin"><span class="hide-text">Linkedin</span></a></li>
+							<li><a href="http://www.facebook.com/share.php?u=<?php echo get_the_permalink(); ?>&title=<?php echo get_the_title(); ?>" class="social-icon icon-facebook"><span class="hide-text">Facebook</span></a></li>
+							<li><a href="http://www.tumblr.com/share?v=3&u=<?php echo get_the_permalink(); ?>&t=<?php echo get_the_title(); ?>" class="social-icon icon-tumblr"><span class="hide-text">Tumblr</span></a></li>
+							<li><a href="https://plus.google.com/share?url=<?php echo get_the_permalink(); ?>" class="social-icon icon-googleplus"><span class="hide-text">Google Plus</span></a></li>
 						</ul>
 					</div>
 				</aside>
