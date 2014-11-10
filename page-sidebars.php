264250c9c68d61490eed9bda5330da2a2358c437
Dave Kellam <dave@eightface.com>
2014-11-10 01:16:12 -0500

    Formatting in page-sidebars

--- page-sidebars.php
+++ page-sidebars.php
@@ -9,38 +9,38 @@ $post_sidebar_1 = get_post_meta( $post->ID, 'post_sidebar_1', true );
 $post_sidebar_2 = get_post_meta( $post->ID, 'post_sidebar_2', true );
 ?>
 
-	<div id="primary" class="content-area page-default">
-	
-		<header class="page-header">
-			<div class="span12 aligncenter">
-				<h1 class="page-title"><?php the_title(); ?></h1>
+<div id="primary" class="content-area page-default">
+
+	<header class="page-header">
+		<div class="span12 aligncenter">
+			<h1 class="page-title"><?php the_title(); ?></h1>
+		</div>
+	</header>
+
+	<main id="main" class="site-main span12 aligncenter" role="main">
+
+		<aside class="aside aside-left span2 push-left">
+			<div class="border-block top">
+				<?php echo apply_filters( 'the_content', $post_sidebar_1 ); ?>
 			</div>
-		</header>
+		</aside>
 
-		<main id="main" class="site-main span12 aligncenter" role="main">
+		<section class="span6 push-left">
+			<?php while ( have_posts() ) : the_post(); ?>
 
-			<aside class="aside aside-left span2 push-left">
-				<div class="border-block top">
-					<?php echo apply_filters( 'the_content', $post_sidebar_1 ); ?>
+				<div class="entry-content">
+					<?php the_content(); ?>
 				</div>
-			</aside>
 
-			<section class="span6 push-left">
-				<?php while ( have_posts() ) : the_post(); ?>
+			<?php endwhile; ?>
+		</section>
 
-					<div class="entry-content">
-						<?php the_content(); ?>
-					</div><!-- .entry-content -->
-
-				<?php endwhile; // end of the loop. ?>
-			</section>
-
-			<aside class="aside aside-right span2">
-				<div class="border-block top">
-					<?php echo apply_filters( 'the_content', $post_sidebar_2 ); ?>
-				</div>
-			</aside>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<aside class="aside aside-right span2">
+			<div class="border-block top">
+				<?php echo apply_filters( 'the_content', $post_sidebar_2 ); ?>
+			</div>
+		</aside>
+	</main>
+</div>
 
 <?php get_footer(); ?>
