59cdd2d6ff3cd42b08bfa2a1d53a5f1c908d1a40
Kirk Pettinga <kirkpettinga@Kirks-MacBook-Air.local>
2014-11-11 11:08:02 +0700

    update mobile menu style, js

--- js/core.js
+++ js/core.js
@@ -72,8 +72,54 @@ var Carr = {
 			updateActive(target_class);
 			console.log('!!!');
 		});
-	}
-};
+	},
+	
+	// Handles toggling the navigation menu for small screens.
+	mobileMenuToggle: function() {
+		var container, button, menu;
+	
+		container = document.getElementById( 'site-navigation' );
+		if ( ! container ) {
+			return;
+		}
+	
+		button = container.getElementsByTagName( 'button' )[0];
+		if ( 'undefined' === typeof button ) {
+			return;
+		}
+	
+		menu = container.getElementsByTagName( 'ul' )[0];
+	
+		// Hide menu toggle button if menu is empty and return early.
+		if ( 'undefined' === typeof menu ) {
+			button.style.display = 'none';
+			return;
+		}
+	
+		menu.setAttribute( 'aria-expanded', 'false' );
+	
+		if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
+			menu.className += ' nav-menu';
+		}
+	
+		button.onclick = function() {
+			if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+				container.className = container.className.replace( ' toggled', '' );
+				button.setAttribute( 'aria-expanded', 'false' );
+				$(button).text('Menu');
+				menu.setAttribute( 'aria-expanded', 'false' );
+			} else {
+				container.className += ' toggled';
+				button.setAttribute( 'aria-expanded', 'true' );
+				$(button).text('Close');
+				menu.setAttribute( 'aria-expanded', 'true' );
+			}
+		};
+	},
+	
+	
+}; // Carr object
+
 
 jQuery(function ($) {
 	window.$ = $;
@@ -86,47 +132,6 @@ jQuery(function ($) {
 
 	// Find a body class to make sure this only gets setup there
 	Carr.setupBottomMatter();
+	
+	Carr.mobileMenuToggle();
 });
-
-/**
- * Handles toggling the navigation menu for small screens.
- */
-( function() {
-	var container, button, menu;
-
-	container = document.getElementById( 'site-navigation' );
-	if ( ! container ) {
-		return;
-	}
-
-	button = container.getElementsByTagName( 'button' )[0];
-	if ( 'undefined' === typeof button ) {
-		return;
-	}
-
-	menu = container.getElementsByTagName( 'ul' )[0];
-
-	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
-		button.style.display = 'none';
-		return;
-	}
-
-	menu.setAttribute( 'aria-expanded', 'false' );
-
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
-		menu.className += ' nav-menu';
-	}
-
-	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
-			container.className = container.className.replace( ' toggled', '' );
-			button.setAttribute( 'aria-expanded', 'false' );
-			menu.setAttribute( 'aria-expanded', 'false' );
-		} else {
-			container.className += ' toggled';
-			button.setAttribute( 'aria-expanded', 'true' );
-			menu.setAttribute( 'aria-expanded', 'true' );
-		}
-	};
-} )();
