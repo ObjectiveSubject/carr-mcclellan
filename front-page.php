70279747521622cef68e52de976dfa9a41dd199d
Kirk Pettinga <kirk.pettinga@gmail.com>
2014-11-14 14:29:19 +0700

    Front page style tweaks

--- front-page.php
+++ front-page.php
@@ -56,14 +56,14 @@ $home_featured = z_get_zone_query( 'home-featured' );
 	<main id="main" class="site-main span12 aligncenter" role="main">
 
 		<section class="news-events span10 aligncenter">
-			<h2 class="front-page">News &amp; Events</h2>
+			<h3 class="front-page">News &amp; Events</h2>
 			<a href="<?php echo get_bloginfo( 'url' ); ?>/news-events/" class="see-all">See All <span class="icon-arrow-right"></span></a>
 
 			<?php cmc_get_newsevents(); ?>
 		</section>
 
 		<section class="attorneys span10 aligncenter">
-			<h2 class="front-page">Meet Our Attorneys</h2>
+			<h3 class="front-page">Meet Our Attorneys</h2>
 			<a href="<?php echo get_bloginfo( 'url' ); ?>/attorneys/" class="see-all">See All <span class="icon-arrow-right"></span></a>
 
 			<?php cmc_get_attorneys(); ?>
@@ -71,7 +71,7 @@ $home_featured = z_get_zone_query( 'home-featured' );
 		</section>
 
 		<section class="practices span10 aligncenter">
-			<h2 class="front-page">Our Practices</h2>
+			<h3 class="front-page">Our Practices</h2>
 			<?php cmc_get_practices(); ?>
 		</section>
 
