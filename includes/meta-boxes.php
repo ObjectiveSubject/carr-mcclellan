d38189166b2fae65960c295f0aa8e78bc245fb4b
Dave Kellam <dave@eightface.com>
2014-11-18 16:57:15 -0500

    Refactor meta-boxes for coding standards

--- includes/meta-boxes.php
+++ includes/meta-boxes.php
@@ -28,99 +28,107 @@ function setup_meta_boxes() {
 	// Pages
 	add_meta_box( "carr_page_sidebars", "Sidebars", "carr_page_sidebars", "page" );
 }
+
 add_action( 'add_meta_boxes', 'setup_meta_boxes' );
 
 
 /**
  * Meta Box to display Attorneys that can be tagged
  */
-function posts_attorneys_meta_options(){
+function posts_attorneys_meta_options() {
 	global $post;
-	$custom = get_post_custom($post->ID);
-	$post_attorneys = get_post_meta($post->ID, 'post_attorneys', true); ?>
+	$custom         = get_post_custom( $post->ID );
+	$post_attorneys = get_post_meta( $post->ID, 'post_attorneys', true ); ?>
 
-		<div style="background:#fff;border:1px solid #ddd; padding:10px; height:140px;overflow-x;hidden; overflow-y:scroll;">
-			<ul style="margin:0px;">
-				<?php
-					$get_attorneys = get_posts(array(
-							'post_type' => 'attorneys',
-							'meta_key' => 'last_name',
-							'orderby' => 'meta_value',
-							'order' => 'ASC',
-							'numberposts' => 100
-					));
-					
-					foreach ( $get_attorneys as $attorney ) {
-						$name = $attorney->post_title;
-						$id = $attorney->ID;
-						$checked = '';
-						$checked = ( is_array($post_attorneys) && in_array($id, $post_attorneys) ) ? 'checked="checked"' : '';
-						echo '<li>';
-						echo '<input type="checkbox" name="post_attorneys[]" value="' . $id . '" ' . $checked . '/> ' . $name;
-						echo '</li>';
-					}
-				?>
-			</ul>
-		</div>
-		
-<?php }
+	<div style="background:#fff;border:1px solid #ddd; padding:10px; height:140px;overflow-x;hidden; overflow-y:scroll;">
+		<ul style="margin:0px;">
+			<?php
+			$get_attorneys = get_posts( array(
+				'post_type'   => 'attorneys',
+				'meta_key'    => 'last_name',
+				'orderby'     => 'meta_value',
+				'order'       => 'ASC',
+				'numberposts' => 100
+			) );
+
+			foreach ( $get_attorneys as $attorney ) {
+				$name    = $attorney->post_title;
+				$id      = $attorney->ID;
+				$checked = '';
+				$checked = ( is_array( $post_attorneys ) && in_array( $id, $post_attorneys ) ) ? 'checked="checked"' : '';
+				echo '<li>';
+				echo '<input type="checkbox" name="post_attorneys[]" value="' . $id . '" ' . $checked . '/> ' . $name;
+				echo '</li>';
+			}
+			?>
+		</ul>
+	</div>
+
+<?php
+}
 
 /**
  * Save Attorneys associated with post
  *
  * @todo Add nonces/security here
  */
-function save_posts_attorneys_meta(){
+function save_posts_attorneys_meta() {
 	global $post;
-	if(isset($_POST["post_attorneys"])) update_post_meta($post->ID, "post_attorneys", $_POST["post_attorneys"]);
+	if ( isset( $_POST["post_attorneys"] ) ) {
+		update_post_meta( $post->ID, "post_attorneys", $_POST["post_attorneys"] );
+	}
 }
-add_action('save_post', 'save_posts_attorneys_meta');
 
+add_action( 'save_post', 'save_posts_attorneys_meta' );
 
 
 /**
  * Meta Box to display Pratices that can be tagged
  */
-function posts_practices_meta_options(){
+function posts_practices_meta_options() {
 	global $post;
-	$custom = get_post_custom($post->ID);
-	$post_practices = get_post_meta($post->ID, 'post_practices', true); ?>
+	$custom         = get_post_custom( $post->ID );
+	$post_practices = get_post_meta( $post->ID, 'post_practices', true ); ?>
 
 	<div style="background:#fff;border:1px solid #ddd; padding:10px; height:140px;overflow-x;hidden; overflow-y:scroll;">
 		<ul style="margin:0px;">
 			<?php
-				$get_practices = get_posts(array(
-						'post_type' => 'practices',
-						//'meta_key' => 'last_name',
-						//'orderby' => 'meta_value',
-						'order' => 'ASC',
-						'numberposts' => 100
-				));
-				
-				foreach ( $get_practices as $practices ) {
-					$name = $practices->post_title;
-					$id = $practices->ID;
-					$checked = '';
-					$checked = (is_array($post_practices) && in_array($id, $post_practices)) ? 'checked="checked"' : '';
-					echo '<li>';
-					echo '<input type="checkbox" name="post_practices[]" value="' . $id . '" ' . $checked . '/> ' . $name;
-					echo '</li>';
-				} ?>
+			$get_practices = get_posts( array(
+				'post_type'   => 'practices',
+				//'meta_key' => 'last_name',
+				//'orderby' => 'meta_value',
+				'order'       => 'ASC',
+				'numberposts' => 100
+			) );
+
+			foreach ( $get_practices as $practices ) {
+				$name    = $practices->post_title;
+				$id      = $practices->ID;
+				$checked = '';
+				$checked = ( is_array( $post_practices ) && in_array( $id, $post_practices ) ) ? 'checked="checked"' : '';
+				echo '<li>';
+				echo '<input type="checkbox" name="post_practices[]" value="' . $id . '" ' . $checked . '/> ' . $name;
+				echo '</li>';
+			} ?>
 		</ul>
 	</div>
-	
-<?php }
+
+<?php
+}
 
 /**
  * Save Practices associated with post
  *
  * @todo Add nonces/security here
  */
-function save_posts_practices_meta(){
+function save_posts_practices_meta() {
 	global $post;
-	if(isset($_POST["post_practices"])) update_post_meta($post->ID, "post_practices", $_POST["post_practices"]);
+	if ( isset( $_POST["post_practices"] ) ) {
+		update_post_meta( $post->ID, "post_practices", $_POST["post_practices"] );
+	}
 }
-add_action('save_post', 'save_posts_practices_meta');
+
+add_action( 'save_post', 'save_posts_practices_meta' );
 
 
 // add_action( 'edit_form_after_title', 'carr_subtitle' );
@@ -130,7 +138,7 @@ function carr_subtitle( $post ) {
 		return;
 	}
 
-	$post_subtitle = get_post_meta( $post->ID, 'post_subtitle',true );
+	$post_subtitle = get_post_meta( $post->ID, 'post_subtitle', true );
 	?>
 	<div class="edit-form-section">
 		<label for="post_subtitle">Subtitle:</label>
@@ -143,13 +151,19 @@ function carr_subtitle( $post ) {
 
 function save_post_subtitle( $post_id, $post ) {
 	// Bail if doing autosave
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) { return; }
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
 
 	// Bail if nonce isn't set
-	if ( ! isset( $_POST['carr_page_meta'] ) || ! wp_verify_nonce( $_POST['carr_page_meta'], 'save' ) ) { return; }
+	if ( ! isset( $_POST['carr_page_meta'] ) || ! wp_verify_nonce( $_POST['carr_page_meta'], 'save' ) ) {
+		return;
+	}
 
 	// Bail if the user isn't allowed to edit the post
-	if ( ! current_user_can( 'edit_post', $post_id ) ) { return; }
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
 
 	// Bail if not a page
 	// if ( 'attorneys' !== $post->post_type ) { return; }
@@ -158,12 +172,14 @@ function save_post_subtitle( $post_id, $post ) {
 		update_post_meta( $post->ID, "post_subtitle", $_POST["post_subtitle"] );
 	}
 }
+
 add_action( 'save_post', 'save_post_subtitle', 15, 2 );
 
 /**
  * Used for the display date box
  *
  * @param $post
+ *
  * @todo rename to be more semantic
  */
 function carr_post_options( $post ) {
@@ -172,7 +188,7 @@ function carr_post_options( $post ) {
 	}
 
 	$original_event_date = get_post_meta( $post->ID, 'date', true );
-	$display_date = get_post_meta( $post->ID, 'display_date', true );
+	$display_date        = get_post_meta( $post->ID, 'display_date', true );
 	$display_date_manual = get_post_meta( $post->ID, 'display_date_manual', true );
 
 
@@ -183,7 +199,7 @@ function carr_post_options( $post ) {
 	// This JS could probably be moved into an admin script
 	?>
 	<script type="text/javascript">
-		jQuery(document).ready(function($) {
+		jQuery(document).ready(function ($) {
 			$("input[name='display_date']").datepicker({
 				dateFormat: 'mm/dd/yy',
 				changeYear: true
@@ -196,7 +212,9 @@ function carr_post_options( $post ) {
 		<div class="display-date-group display-date">
 			<label for="display_date"><strong>Date Picker:</strong></label><br>
 			<input id="display_date" name="display_date" value="<?php echo $display_date; ?>">
+
 			<p>Click the box to select your date</p>
+
 			<p><a class="datepicker-toggle">Use custom date instead</a></p>
 		</div>
 
@@ -205,6 +223,7 @@ function carr_post_options( $post ) {
 			<input id="display_date_manual" name="display_date_manual" value="<?php echo $display_date_manual; ?>">
 
 			<p>Use this to override the date with your own text (Spring 2012, May 2014, etc.)</p>
+
 			<p><a class="datepicker-toggle">Use date picker instead</a></p>
 		</div>
 
@@ -225,9 +244,9 @@ function carr_post_news_options( $post ) {
 
 	global $post;
 
-	$news_type_select = get_post_meta($post->ID, "news_type_select", true );
-	$source = get_post_meta($post->ID, "source", true );
-	$source_url = get_post_meta($post->ID, "source_url", true );
+	$news_type_select = get_post_meta( $post->ID, "news_type_select", true );
+	$source           = get_post_meta( $post->ID, "source", true );
+	$source_url       = get_post_meta( $post->ID, "source_url", true );
 
 	?>
 
@@ -237,9 +256,18 @@ function carr_post_news_options( $post ) {
 			<td>
 				<select name="news_type_select">
 					<option value=""></option>
-					<option value="internal" <?php if($news_type_select == 'internal'){echo 'selected="selected"';}?>>Press Release (Internal)</option>
-					<option value="external" <?php if($news_type_select == 'external'){echo 'selected="selected"';}?>>External Site</option>
-					<option value="pdf" <?php if($news_type_select == 'pdf'){echo 'selected="selected"';}?>>PDF</option>
+					<option value="internal" <?php if ( $news_type_select == 'internal' ) {
+						echo 'selected="selected"';
+					} ?>>Press Release (Internal)
+					</option>
+					<option value="external" <?php if ( $news_type_select == 'external' ) {
+						echo 'selected="selected"';
+					} ?>>External Site
+					</option>
+					<option value="pdf" <?php if ( $news_type_select == 'pdf' ) {
+						echo 'selected="selected"';
+					} ?>>PDF
+					</option>
 				</select>
 			</td>
 		</tr>
@@ -254,7 +282,8 @@ function carr_post_news_options( $post ) {
 			<th style="width: 20%;"><label for="source_url">Source URL</label></th>
 			<td>
 				<input type="text" class="text_input" name="source_url" id="source_url" value="<?php echo $source_url; ?>" style="width:100%" /><br />
-				<small>Ex: http://www.google.com/</small><br />
+				<small>Ex: http://www.google.com/</small>
+				<br />
 				<!-- <small>Note: If PDF file is attached below, this link will be overriden with a link to the PDF.</small> -->
 			</td>
 		</tr>
@@ -274,8 +303,8 @@ function carr_page_sidebars( $post ) {
 		return;
 	}
 
-	$post_sidebar_1 = get_post_meta( $post->ID, 'post_sidebar_1',true );
-	$post_sidebar_2 = get_post_meta( $post->ID, 'post_sidebar_2',true );
+	$post_sidebar_1 = get_post_meta( $post->ID, 'post_sidebar_1', true );
+	$post_sidebar_2 = get_post_meta( $post->ID, 'post_sidebar_2', true );
 	?>
 	<div class="edit-form-section">
 		<label for="post_sidebar_1">Sidebar 1:</label>
@@ -298,20 +327,29 @@ function carr_page_sidebars( $post ) {
  *
  * @param $post_id
  * @param $post
+ *
  * @todo rename to make this more semantic
  */
 function save_post_options_data( $post_id, $post ) {
 	// Bail if doing autosave
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) { return; }
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
 
 	// Bail if nonce isn't set
-	if ( ! isset( $_POST['carr_post_event_data'] ) || ! wp_verify_nonce( $_POST['carr_post_event_data'], 'save' ) ) { return; }
+	if ( ! isset( $_POST['carr_post_event_data'] ) || ! wp_verify_nonce( $_POST['carr_post_event_data'], 'save' ) ) {
+		return;
+	}
 
 	// Bail if the user isn't allowed to edit the post
-	if ( ! current_user_can( 'edit_post', $post_id ) ) { return; }
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
 
 	// Bail if not a page
-	if ( 'post' !== $post->post_type ) { return; }
+	if ( 'post' !== $post->post_type ) {
+		return;
+	}
 
 	if ( isset( $_POST["display_date"] ) ) {
 		update_post_meta( $post->ID, "display_date", $_POST['display_date'] );
@@ -323,6 +361,7 @@ function save_post_options_data( $post_id, $post ) {
 		delete_post_meta( $post->ID, 'display_date_manual' );
 	}
 }
+
 add_action( 'save_post', 'save_post_options_data', 15, 2 );
 
 
@@ -334,36 +373,45 @@ add_action( 'save_post', 'save_post_options_data', 15, 2 );
  */
 function save_post_news_options_data( $post_id, $post ) {
 	// Bail if doing autosave
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) { return; }
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
 
 	// Bail if nonce isn't set
-	if ( ! isset( $_POST['carr_news_options'] ) || ! wp_verify_nonce( $_POST['carr_news_options'], 'save' ) ) { return; }
+	if ( ! isset( $_POST['carr_news_options'] ) || ! wp_verify_nonce( $_POST['carr_news_options'], 'save' ) ) {
+		return;
+	}
 
 	// Bail if the user isn't allowed to edit the post
-	if ( ! current_user_can( 'edit_post', $post_id ) ) { return; }
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
 
 	// Bail if not an asset
-	if ( 'post' !== $post->post_type ) { return; }
+	if ( 'post' !== $post->post_type ) {
+		return;
+	}
 
 	// Save custom fields
-	if( isset( $_POST["news_type_select"] ) ) {
-		update_post_meta($post->ID, "news_type_select", $_POST["news_type_select"]);
+	if ( isset( $_POST["news_type_select"] ) ) {
+		update_post_meta( $post->ID, "news_type_select", $_POST["news_type_select"] );
 	} else {
-		update_post_meta($post->ID, "news_type_select", '' );
+		update_post_meta( $post->ID, "news_type_select", '' );
 	}
 
-	if( isset( $_POST["source"] ) ) {
-		update_post_meta($post->ID, "source", $_POST["source"]);
+	if ( isset( $_POST["source"] ) ) {
+		update_post_meta( $post->ID, "source", $_POST["source"] );
 	} else {
-		update_post_meta($post->ID, "source", '' );
+		update_post_meta( $post->ID, "source", '' );
 	}
 
-	if( isset( $_POST["source_url"] ) ) {
-		update_post_meta($post->ID, "source_url", $_POST["source_url"]);
+	if ( isset( $_POST["source_url"] ) ) {
+		update_post_meta( $post->ID, "source_url", $_POST["source_url"] );
 	} else {
-		update_post_meta($post->ID, "source_url", '' );
+		update_post_meta( $post->ID, "source_url", '' );
 	}
 }
+
 add_action( 'save_post', 'save_post_news_options_data', 15, 2 );
 
 
@@ -375,16 +423,24 @@ add_action( 'save_post', 'save_post_news_options_data', 15, 2 );
  */
 function save_page_sidebar( $post_id, $post ) {
 	// Bail if doing autosave
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) { return; }
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
 
 	// Bail if nonce isn't set
-	if ( ! isset( $_POST['carr_post_sidebars'] ) || ! wp_verify_nonce( $_POST['carr_post_sidebars'], 'save' ) ) { return; }
+	if ( ! isset( $_POST['carr_post_sidebars'] ) || ! wp_verify_nonce( $_POST['carr_post_sidebars'], 'save' ) ) {
+		return;
+	}
 
 	// Bail if the user isn't allowed to edit the post
-	if ( ! current_user_can( 'edit_post', $post_id ) ) { return; }
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
 
 	// Bail if not a page
-	if ( 'page' !== $post->post_type ) { return; }
+	if ( 'page' !== $post->post_type ) {
+		return;
+	}
 
 	if ( isset( $_POST["post_sidebar_1"] ) ) {
 		update_post_meta( $post->ID, "post_sidebar_1", $_POST["post_sidebar_1"] );
@@ -393,4 +449,5 @@ function save_page_sidebar( $post_id, $post ) {
 		update_post_meta( $post->ID, "post_sidebar_2", $_POST["post_sidebar_2"] );
 	}
 }
+
 add_action( 'save_post', 'save_page_sidebar', 15, 2 );
