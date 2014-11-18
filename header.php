9fa00b54f6af452dad9f20fef64430ab2a33997b
Kirk Pettinga <kirk.pettinga@gmail.com>
2014-11-18 16:03:27 +0700

    added favicon and apple touch icons

--- header.php
+++ header.php
@@ -13,6 +13,17 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
+
+<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/images/icons/favicon.ico" />
+<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/images/icons/favicon@2x.png" />
+
+<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/icons/icon-120x120.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/images/icons/icon-180x180.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/icons/icon-114x114.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/images/icons/icon-76x76.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/images/icons/icon-152x152.png" />
+<link rel="apple-touch-icon" sizes="58x58" href="<?php echo get_template_directory_uri(); ?>/images/icons/icon-58x58.png" />
+
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link rel="stylesheet" type="text/css" href="//cloud.typography.com/6428112/749386/css/fonts.css" />
