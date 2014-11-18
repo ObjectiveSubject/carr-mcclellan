274412e51c5962779448939b03438835a1b9eb26
Dave Kellam <dave@eightface.com>
2014-11-18 17:23:12 -0500

    Remove unused functions

--- content.php
+++ content.php
@@ -7,12 +7,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php carr_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -31,8 +25,4 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php carr_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
--- includes/template-tags.php
+++ includes/template-tags.php
@@ -88,58 +88,6 @@ function carr_post_nav() {
 
 
 /**
- * Prints HTML with meta information for the current post-date/time and author.
- */
-function carr_posted_on() {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
-
-	$time_string = sprintf( $time_string,
-				esc_attr( get_the_date( 'c' ) ),
-				esc_html( get_the_date() ),
-				esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
-
-	$posted_on = sprintf(
-		_x( 'Posted on %s', 'post date', 'cmc' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
-
-	$byline = sprintf(
-		_x( 'by %s', 'post author', 'cmc' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
-
-}
-
-
-/**
- * Prints HTML with meta information for the categories, tags and comments.
- */
-function carr_entry_footer() {
-	// Hide category and tag text for pages.
-	if ( 'post' == get_post_type() ) {
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( __( ', ', 'cmc' ) );
-			if ( $categories_list ) {
-					printf( '<span class="cat-links">' . __( 'Posted in %1$s', 'cmc' ) . '</span>', $categories_list );
-			}
-
-	/* translators: used between list items, there is a space after the comma */
-	$tags_list = get_the_tag_list( '', __( ', ', 'cmc' ) );
-	if ( $tags_list ) {
-					printf( '<span class="tags-links">' . __( 'Tagged %1$s', 'cmc' ) . '</span>', $tags_list );
-				}
-	}
-}
-
-
-/**
  * Generate sidebar sharing links for Posts/Articles
  */
 function carr_share_links() {
