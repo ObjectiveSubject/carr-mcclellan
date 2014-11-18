d4d738fca80691fb55411bc89012ea50d8f39d1a
Dave Kellam <dave@eightface.com>
2014-11-18 17:30:43 -0500

    Code formatting

--- includes/admin.php
+++ includes/admin.php
@@ -4,23 +4,25 @@
  */
 
 /**
-* Remove unused menus from the Dashboard
-*/
+ * Remove unused menus from the Dashboard
+ */
 function carr_remove_menus() {
-remove_menu_page( 'link-manager.php' );
-remove_menu_page( 'edit-comments.php' );
+	remove_menu_page( 'link-manager.php' );
+	remove_menu_page( 'edit-comments.php' );
 }
+
 add_action( 'admin_menu', 'carr_remove_menus' );
 
 /**
-* Move "Media" further down in sidebar
-*/
-function carr_move_media_menu () {
-global $menu;
+ * Move "Media" further down in sidebar
+ */
+function carr_move_media_menu() {
+	global $menu;
 
-$menu[24] = $menu[10];
-unset( $menu[10] );
+	$menu[24] = $menu[10];
+	unset( $menu[10] );
 }
+
 add_action( 'admin_menu', 'carr_move_media_menu' );
 
 
@@ -35,17 +37,20 @@ function milestones_edit_columns( $columns ) {
 
 	return $columns;
 }
+
 add_filter( 'manage_edit-milestones_columns', 'milestones_edit_columns' );
 
 
 /**
  * Custom column headers for Industries
  */
-function industries_edit_columns( $columns ){
+function industries_edit_columns( $columns ) {
 	$columns = array(
-		"cb" => "<input type=\"checkbox\" />",
+		"cb"    => "<input type=\"checkbox\" />",
 		"title" => "Industry Title",
 	);
+
 	return $columns;
 }
+
 add_filter( "manage_edit-industries_columns", "industries_edit_columns" );
