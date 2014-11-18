e582a7453bb43aa467b66c4f30b9137a68525d3d
Dave Kellam <dave@eightface.com>
2014-11-18 17:15:07 -0500

    Moving the one function in extras.php to functions.php

--- functions.php
+++ functions.php
@@ -7,8 +7,6 @@
 define( 'CARR_VERSION', '0.1' );
 define( 'CARR_PATH', dirname( __FILE__ ) . '/' );
 
-include CARR_PATH . 'includes/template-tags.php';
-include CARR_PATH . 'includes/extras.php';
 include CARR_PATH . 'includes/admin.php';
 include CARR_PATH . 'includes/meta-boxes.php';
 include CARR_PATH . 'includes/meta-boxes-articles.php';
@@ -16,6 +14,7 @@ include CARR_PATH . 'includes/meta-boxes-attorneys.php';
 include CARR_PATH . 'includes/meta-boxes-industries.php';
 include CARR_PATH . 'includes/meta-boxes-practices.php';
 include CARR_PATH . 'includes/post-types.php';
+include CARR_PATH . 'includes/template-tags.php';
 include CARR_PATH . 'includes/v_card.php';
 
 /**
@@ -347,3 +346,53 @@ function carr_redirect() {
 	}
 }
 add_action( 'template_redirect', 'carr_redirect' );
+
+/**
+ * Filters wp_title to print a neat <title> tag based on what is being viewed.
+ *
+ * @param string $title Default title text for current view.
+ * @param string $sep Optional separator.
+ * @return string The filtered title.
+ */
+function carr_wp_title( $title, $sep ) {
+	if ( is_feed() ) {
+		return $title;
+	}
+
+	global $page, $paged, $wp;
+
+	// Add the blog name
+	$title .= get_bloginfo( 'name', 'display' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) ) {
+		$title .= " $sep $site_description";
+	}
+
+	// Add a page number if necessary:
+	if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
+		$title .= " $sep " . sprintf( __( 'Page %s', 'cmc' ), max( $paged, $page ) );
+	}
+
+	$query_vars = $wp->query_vars;
+
+	if ( isset( $query_vars['attorney'] ) ) {
+		$attorney    = $query_vars['attorney'];
+		$attorney_id = carr_get_post_id_by_slug( $attorney, 'attorneys' );
+		$attorney_post = get_post( $attorney_id );
+
+		$title .= ' | News &amp; Events | ' . $attorney_post->post_title;
+	}
+
+	if ( isset( $query_vars['practice'] ) ) {
+		$practice    = $query_vars['practice'];
+		$practice_id = carr_get_post_id_by_slug( $practice, 'practices' );
+		$practice_post = get_post( $practice_id );
+
+		$title .= ' | News &amp; Events | ' . $practice_post->post_title;
+	}
+
+	return $title;
+}
+add_filter( 'wp_title', 'carr_wp_title', 10, 2 );
